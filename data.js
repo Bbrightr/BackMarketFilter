97d8c81166e643e728f868e25e38bfdb90f3b88d
Bright Obiri Gyan <gyanobiri@gmail.com>
2026-08-08 15:42:34 +0200

    Building the outline of how must look
    Getting the html and css looks together.
    
    images gallery updated
    CSS updated
    html skeleton also updated
    
    What I'm working on now:
    - Product Listing
    - Product Cards must look like what's in Figma
    - Filter section to be dropdown
    - JavaScript keeping everything together
    - Fix the movement of the header when cursor hovers of it

--- data.js
+++ data.js
@@ -1,63 +1,63 @@
 export const smartPhones = [
         { tag: 'iphone', 
         name: 'iPhone 15 Pro', 
-        photo: 'img/iphone15pro.jpg',
+        photo: 'img/iphone15pro.jpeg',
         price: 1099, 
         year: 2023, 
         stars: 4.7 },
     
         { tag: 'iphone', 
         name: 'iPhone 14', 
-        photo: 'img/iphone14.jpg',
+        photo: 'img/iphone14.jpeg',
         price: 799, 
         year: 2022, 
         stars: 4.4 },
     
         { tag: 'iphone', 
         name: 'iPhone SE (2022)', 
-        photo: 'img/iphonese2022.jpg',
+        photo: 'img/iphonese2022.jpeg',
         price: 429, 
         year: 2022, 
         stars: 4.1 },
     
         { tag: 'samsung', 
         name: 'Galaxy S24 Ultra',
-        photo: 'img/galaxys24ultra.jpg', 
+        photo: 'img/galaxys24ultra.jpeg', 
         price: 1199, 
         year: 2024, 
         stars: 4.6 },
     
         { tag: 'samsung', 
         name: 'Galaxy S23', 
-        photo: 'img/galaxys23.jpg',
+        photo: 'img/galaxys23.jpeg',
         price: 899, 
         year: 2023, 
         stars: 4.3 },
     
         { tag: 'samsung', 
         name: 'Galaxy A54', 
-        photo: 'img/galaxya54.jpg',
+        photo: 'img/galaxya54.jpeg',
         price: 449, 
         year: 2023, 
         stars: 4.2 },
     
         { tag: 'android', 
         name: 'Google Pixel 8', 
-        photo: 'img/pixel8.jpg',
+        photo: 'img/pixel8.jpeg',
         price: 699, 
         year: 2023, 
         stars: 4.5 },
     
         { tag: 'android', 
         name: 'OnePlus 12', 
-        photo: 'img/oneplus12.jpg',
+        photo: 'img/oneplus12.jpeg',
         price: 799, 
         year: 2024, 
         stars: 4.4 },
     
         { tag: 'android', 
         name: 'Xiaomi 14', 
-        
+        photo: 'img/xiaomi14.jpeg',
         price: 749, 
         year: 2024, 
         stars: 4.2 }
@@ -68,7 +68,7 @@ export const smartPhones = [
     {
         tags: 'Windows',
         name: 'Dell XPS 15',
-        photo: 'img/dellxps15.jpg',
+        photo: 'img/dellxps15.jpeg',
         price: 1499,
         year: 2024,
         stars: 4.7
@@ -77,7 +77,7 @@ export const smartPhones = [
     {
         tags: 'Windows',
         name: 'HP Spectre x360',
-        photo: 'img/hpspectrex360.jpg',
+        photo: 'img/hpspectrex360.jpeg',
         price: 1299,
         year: 2024,
         stars: 4.5
@@ -86,7 +86,7 @@ export const smartPhones = [
     {
         tags: 'Windows',
         name: 'Lenovo ThinkPad X1 Carbon',
-        photo: 'img/thinkpadx1carbon.jpg',
+        photo: 'img/thinkpadx1carbon.jpeg',
         price: 1599,
         year: 2023,
         stars: 4.6
@@ -95,7 +95,7 @@ export const smartPhones = [
     {
         tags: 'Macbook',
         name: 'MacBook Pro 16-inch',
-        photo: 'img/macbookpro16.jpg',
+        photo: 'img/macbookpro16.jpeg',
         price: 2499,
         year: 2024,
         stars: 4.8
@@ -104,7 +104,7 @@ export const smartPhones = [
     {
         tags: 'Macbook',
         name: 'MacBook Air M2',
-        photo: 'img/macbookairm2.jpg',
+        photo: 'img/macbookairm2.jpeg',
         price: 1199,
         year: 2023,
         stars: 4.7
@@ -113,7 +113,7 @@ export const smartPhones = [
     {
         tags: 'Macbook',
         name: 'MacBook Pro 14-inch',
-        photo: 'img/macbookpro14.jpg',
+        photo: 'img/macbookpro14.jpeg',
         price: 1999,
         year: 2023,
         stars: 4.8
@@ -122,7 +122,7 @@ export const smartPhones = [
     {
         tags: 'ChromeBook',
         name: 'Google Pixelbook Go',
-        photo: 'img/pixelbookgo.jpg',
+        photo: 'img/pixelbookgo.jpeg',
         price: 849,
         year: 2024,
         stars: 4.3
@@ -131,7 +131,7 @@ export const smartPhones = [
     {
         tags: 'ChromeBook',
         name: 'Acer Chromebook Spin 713',
-        photo: 'img/acerchromebookspin713.jpg',
+        photo: 'img/acerchromebookspin713.jpeg',
         price: 699,
         year: 2023,
         stars: 4.4
@@ -140,7 +140,7 @@ export const smartPhones = [
     {
         tags: 'ChromeBook',
         name: 'ASUS Chromebook Flip C434',
-        photo: 'img/asuschromebookflipc434.jpg',
+        photo: 'img/asuschromebookflipc434.jpeg',
         price: 599,
         year: 2023,
         stars: 4.2
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import { smartPhones, laptops } from './data.js';
+
+const smartphoneCategory = document.getElementById('smartphone-category');
+const smartPhoneModal = document.getElementById('smartphones-modal');
+
+smartphoneCategory.addEventListener('click', showSmartphoneModal);
+
+function showSmartphoneModal() {
+    smartPhoneModal.style.display = 'block';
+}
