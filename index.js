c92ab8702c757c3b2313d50edce523c0ac39690c
Bright Obiri Gyan <gyanobiri@gmail.com>
2026-08-11 23:15:35 +0200

    Updated

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import { smartPhones, laptops } from './data.js';
 const smartphoneCategory = document.getElementById('smartphone-category');
 const laptopCategory = document.getElementById('laptop-category');
 
+
 const smartphonesModal = document.getElementById('smartphones-modal');
 const laptopsModal = document.getElementById('laptops-modal');
 
 
 
-smartphoneCategory.addEventListener('onFiltersReceived', displaySmartphones);
-
-function displaySmartphones() {
-    smartphonesModal.classList.remove('hide');}
+smartphoneCategory.addEventListener('click', function(){
+    smartphonesModal.classList.style.display = "none"
+})
 
 
 
