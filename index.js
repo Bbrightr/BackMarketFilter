5cc8e5f8499fc064098f593a9a894f292316a090
Bright Obiri Gyan <gyanobiri@gmail.com>
2026-08-13 23:09:34 +0200

    restarted build from ground zero. building one feature at a goal

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const smartphoneCategory = document.getElementById('smartphone-category');
 const laptopCategory = document.getElementById('laptop-category');
 
 
-const smartphonesModal = document.getElementById('smartphones-modal');
-const laptopsModal = document.getElementById('laptops-modal');
+smartphoneCategory.addEventListener('mouseover', function(){
+    const categoryDrop = `<div class="category-modal" id="smartphones-modal">
+            <h5 class="modal-title">Categories</h5>
+            <div class="smartphones-modal" >
+                <div class="sub-category">
+                    <img src="img/iphones.png" alt="iPhones">
+                    <p>iPhones</p>
+                </div>
+                <div class="sub-category">
+                    <img src="./img/samsung.png" alt="Samsung Galaxy">
+                    <p>Samsung</p>
+                </div>
+                <div class="sub-category">
+                    <img src="./img/otherBrands.png" alt="Other Brands">
+                    <p>Other Brands</p>
+                </div>
+            </div>
+        </div>`
 
-
-
-smartphoneCategory.addEventListener('click', function(){
-    smartphonesModal.classList.style.display = "none"
+    
 })
 
-
-
-
+laptopCategory.addEventListener('mouseover', function(e){
+    console.log(e)
+})
