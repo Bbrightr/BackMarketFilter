4c4ae41bd23fa9d820db50d7092e9337bb63802e
Bright Obiri Gyan <gyanobiri@gmail.com>
2026-08-17 09:25:29 +0200

    fixed some errors
    quirk mode in browser console fixed
    addEventListener on nav-bar is now working

--- index.js
+++ index.js
@@ -6,29 +6,37 @@ const laptopCategory = document.getElementById('laptop-category');
 const smartphoneModal = document.getElementById('smartphones-modal')
 
 
-smartphoneCategory.addEventListener('mouseover', function(){
-    smartphoneModal.innerHTML = `
-        <div class="category-modal" id="smartphones-modal">
-            <h5 class="modal-title">Categories</h5>
-            <div class="smartphones-modal" >
-                <div class="sub-category">
-                    <img src="img/iphones.png" alt="iPhones">
-                    <p>iPhones</p>
-                </div>
-                <div class="sub-category">
-                    <img src="./img/samsung.png" alt="Samsung Galaxy">
-                    <p>Samsung</p>
-                </div>
-                <div class="sub-category">
-                    <img src="./img/otherBrands.png" alt="Other Brands">
-                    <p>Other Brands</p>
-                </div>
-            </div>
-        </div>`
+document.addEventListener('mouseover', function(e){
+    if (e.target.id === "smartphone-category") {
+        console.log("mouse moved over Smartphones");
+    } else if (e.target.id === "laptop-category"){
+        console.log("mouse moved over laptop-category")
+    }
+})
+
+// smartphoneCategory.addEventListener('mouseover', function(){
+//     smartphoneModal.innerHTML = `
+//         <div class="category-modal" id="smartphones-modal">
+//             <h5 class="modal-title">Categories</h5>
+//             <div class="smartphones-modal" >
+//                 <div class="sub-category">
+//                     <img src="img/iphones.png" alt="iPhones">
+//                     <p>iPhones</p>
+//                 </div>
+//                 <div class="sub-category">
+//                     <img src="./img/samsung.png" alt="Samsung Galaxy">
+//                     <p>Samsung</p>
+//                 </div>
+//                 <div class="sub-category">
+//                     <img src="./img/otherBrands.png" alt="Other Brands">
+//                     <p>Other Brands</p>
+//                 </div>
+//             </div>
+//         </div>`
 
     
-})
+// })
 
-laptopCategory.addEventListener('mouseover', function(e){
-    console.log(e)
-})
+// laptopCategory.addEventListener('mouseover', function(e){
+//     console.log(e)
+// })
